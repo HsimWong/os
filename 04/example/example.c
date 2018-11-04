0aa047054870e1c87f92fc9fff6bbb6b321d43bf
HsimWong <16074101@emails.bjut.edu.cn>
2018-11-04 09:18:52 -0800

    midnight

--- 04/example/example.c
+++ 04/example/example.c
@@ -5,20 +5,17 @@
 sem_t s;
 
 void thread1(){
-	printf("This is thread1\n");
-	printf("s before wait = %d\n", s);
+
 	sem_wait(&s);
-	printf("s after wait = %d\n", s);
+	printf("This is thread1\n");
 	
 	return;
 }
 
 void thread2(){
 	printf("This is thread2\n");
-	printf("s before post = %d\n", s);
-
+	
 	sem_post(&s);
-	printf("s after post = %d\n", s);
 	return;
 }
 
@@ -29,7 +26,7 @@ int main(int argc, char const *argv[])
 	pthread_t id_2;
 	pthread_create(&id_1, NULL, (void *)thread1, NULL);
 	pthread_create(&id_2, NULL, (void *)thread2, NULL);
-	sleep(3);
+	// sleep(3);
 	pthread_join(id_2, NULL);
 	pthread_join(id_1, NULL);
 	/* code */
--- 04/semaphore.c
+++ 04/semaphore.c
@@ -8,35 +8,156 @@ sem_t need;
 sem_t need_0;
 sem_t need_1;
 
+sem_t rd_0;
+sem_t rd_1;
+
+int have_num_0;
+int have_num_1;
+
 int get_sem_val(sem_t * sem){
+
 	int sval;
 	sem_getvalue(sem, &sval);
 	return sval;
 }
 
-void read_1(){
+void * read_0(){
+	printf("read_0\n");
 	int temp;
 	FILE * fp = fopen("1.dat", "r");
 	if(!fp){
-		printf("File open error");
-		return;
+		printf("File \"1.dat\"open error");
+		return (void *)0;
 	}
 	
 	for(int i = 0; !feof(fp); i++){
+		have_num_0 = 1;
 		sem_wait(&need);
 		fscanf(fp, "%d", &temp);
-	//	printf("%d", temp);
-	//	if()			
+		if(get_sem_val(&need_0)){
+			sem_wait(&need_0);
+			buf[0] = temp;
+			sem_post(&rd_0);
+		}	
+		else{
+			sem_wait(&need_1);
+			buf[1] = temp;
+			sem_post(&rd_1);
+		}
 	}
+	have_num_0 = 0;
+	return (void *)0;
+
 }
+
+void * read_1(){
+	printf("read_1\n");
+	int temp;
+	FILE * fp = fopen("2.dat", "r");
+	if(!fp){
+		printf("File \"2.dat\"open error");
+		return (void *)0;
+	}
+	
+	for(int i = 0; !feof(fp); i++){
+		have_num_0 = 1;
+		sem_wait(&need);
+		fscanf(fp, "%d", &temp);
+		if(get_sem_val(&need_0)){
+			sem_wait(&need_0);
+			buf[0] = temp;
+			sem_post(&rd_0);
+		}	
+		else{
+			sem_wait(&need_1);
+			buf[1] = temp;
+			sem_post(&rd_1);
+		}
+		
+		for(int i = 0; i < 2; i++){
+			printf("%d\n", buf[i]);
+		}
+	}
+	have_num_0 = 0;
+	return (void *)0;
+
+}
+
+void * multp(){
+	while(have_num_1 || have_num_0){
+		sem_wait(&rd_0);
+		sem_wait(&rd_1);
+		printf("%d * %d = %d\n", buf[0], buf[1], (buf[0] * buf[1]));
+		sem_post(&need_1);
+		sem_post(&need);
+		sem_post(&need_0);
+		sem_post(&need);
+	}
+
+	return (void *)0;
+}
+void * add(){
+	while(have_num_1 || have_num_0){
+		sem_wait(&rd_0);
+		sem_wait(&rd_1);
+		printf("%d + %d = %d\n", buf[0], buf[1], (buf[0] + buf[1]));
+		sem_post(&need_1);
+		sem_post(&need);
+		sem_post(&need_0);
+		sem_post(&need);
+	}
+
+	return (void *)0;
+}
+
 
 
 
 int main(int argc, char const * argv[]){
 	sem_init(&need, 0, 2);
-	printf("%d\n", get_sem_val(&need));
-	sem_wait(&need);
-	// sem_wait(&need);
-	printf("%d\n", get_sem_val(&need));
-	
+	sem_init(&need_0, 0, 1);
+	sem_init(&need_1, 0, 1);
+	pthread_t id_add;
+	pthread_t id_mul;
+	pthread_t id_r_0;
+	pthread_t id_r_1;
+	pthread_create(&id_r_0, NULL, (void *)read_0, NULL);
+	pthread_create(&id_r_1, NULL, (void *)read_1, NULL);
+	pthread_create(&id_add, NULL, (void *)multp, NULL);
+	pthread_create(&id_mul, NULL, (void *)add, NULL);
+	// sleep(5);
+	pthread_join(&id_r_0, NULL);
+	pthread_join(&id_r_1, NULL);
+	pthread_join(&id_add, NULL);
+	pthread_join(&id_mul, NULL);
+	return 0;
+
 }
+
+// Notes Here:
+// 1. Use sem_wait(), sem_post() in stead of implicit wait()
+
+
+// /home/ryan/Desktop/os/04/semaphore.c: In function ‘main’:
+// /home/ryan/Desktop/os/04/semaphore.c:109:2: warning: implicit declaration of function ‘pthread_create’ [-Wimplicit-function-declaration]
+//   pthread_create(&id_r_0, NULL, (void *)read_0, NULL);
+//   ^
+// /home/ryan/Desktop/os/04/semaphore.c:113:2: warning: implicit declaration of function ‘sleep’ [-Wimplicit-function-declaration]
+//   sleep(5);
+//   ^
+// /home/ryan/Desktop/os/04/semaphore.c:114:2: warning: implicit declaration of function ‘pthread_join’ [-Wimplicit-function-declaration]
+//   pthread_join(&id_r_0, NULL);
+//   ^
+// read_1
+// read_0
+// -1 + -2 = -3
+// 1 + -3 = -2
+// 2 + -4 = -2
+// 3 + -5 = -2
+// 4 + -6 = -2
+// 5 + -7 = -2
+// 6 + -8 = -2
+// 7 + -9 = -2
+// 8 + -10 = -2
+// 9 + -10 = -1
+// [Finished in 5.2s]
