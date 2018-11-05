3828f5e9244f8943c0b1212a1fee1a69fc222b0e
ubuntu <ubuntu@localhost.localdomain>
2018-11-05 09:08:31 +0800

    rs first draft

--- 05/msg.c
+++ 05/msg.c
@@ -17,19 +17,10 @@ bool is_exit(char * str){
 	return true;
 }
 
-
-int get_line(char * line, int max_size){
-	char c;
-	int len = 0;
-	while((c = getchar()) != EOF && len < max_size){
-		line[len++] = c;
-		if(c == '\n'){
-			break;
-		}
-	}
+struct msg_st{
+	long int msg_type;
+	char text[256];
 }
-
-
 
 int main(int argc, char const *argv[])
 {
@@ -38,7 +29,23 @@ int main(int argc, char const *argv[])
 
 	// reciever process
 	if(child_0){
-
+		int msg_id = -1;
+		struct msg_st data;
+		msg_id = msgget((key_t)1234, IPC_CREAT | 0666);
+		if(msg_id == -1){
+			printf("msgget failed\n");
+			exit(EXIT_FALURE);
+		}
+		printf("Receiver start\n");
+		while(1){
+			int status = msgrcv(msg_id, (void*)&data, 256, msg_type, 0);
+			if(status == -1){
+				printf("msgrcv failed\n");
+				exit(EXIT_FAILURE);
+			}
+			printf("Message from sender:%s\n", data.text)
+			
+		}
 	}
 
 	// sender process
@@ -46,23 +53,30 @@ int main(int argc, char const *argv[])
 		
 		while(true){
 			int max_size = 256;
-			char * msg = (char *)malloc(sizeof(char) * max_size);
-			// while((len = get_line(msg, max_size))!= 0){
+			// char * msg_1 = "This is a message";
+			// char * msg_2 = "This is another message";
+			char * msg[256];
+			memset(buffer, '\0', 256);
+			//int msg_id = -1;
+			struct msg_st data;
+			long int msg_type = 0;
+			int msg_id = msgget((key_t)1234, IPC_CREAT | 0666);
+			if(msg_id == -1){
+				printf("msgget failed\n");
+				exit(EXIT_FAILURE);
+			}
 
-			// }
-			printf("Entre a message:\n");
-			int len = 0;
-			char c;
-			while((c = getchar()) != EOF && len < max_size){
-				msg[len++] = c;
-				if(c == '\n'){
+			while(1){
+				printf("Enter a string\n");
+				fgets(msg, 256, stdin);
+				strcpy(data.text, msg);
+				data.msg_type = 1;
+				if(is_exit(data.text)){
 					break;
 				}
+				msgsnd(msg_id, (void * )&data, 256, 0);
 			}
 
-			if (is_exit(msg)){
-				break;
-			}
 		}
 
 
