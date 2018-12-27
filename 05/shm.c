2387fafe0d8324bfc3e146a7ddf6a4ee48fe165d
HsimWong <16074101@emails.bjut.edu.cn>
2018-12-27 12:53:36 -0800

    05-02 pre-finished

--- 05/shm.c
+++ 05/shm.c
@@ -7,22 +7,28 @@
 #define SIZE 1024
 
 int main(){
-		int pid = fork();
-		if(pid > 0){
-				int shid = shmget(SHM_KEY, SIZE, IPC_CREATE | 0666);
-				int * addr = shmat(shid, 0, 0);
-				wait(pid);
-				printf("The input number is: %d\n", * addr);
-				// wait(pid);
-				shmdt(addr);
-				shmctl(shid, IPC_RMID, 0);
+	int pid = fork();
+	if(pid > 0){
+		int shid = shmget(SHM_KEY, SIZE, IPC_CREAT | 0666);
+		int * addr = shmat(shid, 0, 0);
+		wait(pid);
+		for(int i = 0; i < 10; i++){
+			printf("%d\n", *(addr + i * sizeof(int)));
 		}
-		else{
-				int shid = shmget(SHM_KEY, SIZE, IPC_CREAT | 0666);
-				int * addr = shmat(shid, 0, 0);
-				printf("Please input:");
-				scanf("%d", addr);
-				sleep(0);
+		shmdt(addr);
+		shmctl(shid, IPC_RMID, 0);
+	}	
+	else{
+		int shid = shmget(SHM_KEY, SIZE, IPC_CREAT | 0666);
+		int * addr = shmat(shid, 0, 0);
+		for (int i = 0; i < 10; i++){
+			printf("Please input the %d number", i);
+			scanf("%d", addr + i * sizeof(int));
 		}
-		return 0;
+		// printf("Please input:");
+		// scanf("%d", addr);
+		sleep(0);
+	}
+	return 0;
 }
+
