08f14bde66b6eaf86fdc111944c5b5e670e3de5e
HsimWong <16074101@emails.bjut.edu.cn>
2018-12-30 08:58:18 -0800

    02 fork.c Notation added

--- 02/fork.c
+++ 02/fork.c
@@ -2,71 +2,55 @@
 #include<unistd.h>
 #include<string.h>
 
-int main(int argc, char const *argv[])
-{
-
-
-	// Determining the real value of getpid() of father.
+int main(int argc, char const *argv[]){
+	/* Here describes the father process
+	 * Use getpid() to get current pid #.*/
 	printf("MainProcess's getpid() = %d\n", getpid());
 
-
-	
-	// printf("%d\n", pid);
-	
-	// After a process is forked, the new forked process is executed first
-
-
-	// In this scope, field[0] is opened for reading
-	// field[1] is opened for writing
-	int field[2];
-	pipe(field);
-	// char buf_write[200];
+	/* Description of establishment of pipe.
+	 * In this scope, field[0] is opened for reading
+	 * while field[1] is opened for writing*/
+	int field[2];			// pipe field[2] declaration
+	pipe(field);			// pipe() for initialization
 	char buf_read[200];
 
+	/* Use fork() function to establish a new process.*/	
 	int pid_1 = fork();
-	int pid_2 = fork();
-	if(pid_1){
-		
+	
+	/* What is known:
+	 * 1. When a new process is forked, pid_1 of son's
+	 * 	  process equals 0, which is false
+	 * 2. Sub-process(son) is processed first*/
+	if(pid_1){	
+		/* Activity declaration of Father's process*/
 		wait(pid_1);
-
 		printf("Parent's getpid() = %d\n", getpid());
-
 		printf("Parent's pid_1 = %d\n", pid_1);
 		close(field[1]);
 		read(field[0], buf_read, 24);
 		printf("%s\n", buf_read);
-		// printf("a\n");
-		
-
 	}
-	else{
-		// printf("b\n");
-		
-		
-		// Here declares what son is doing
-		if(pid_2){
 
+	else{
+		int pid_2 = fork();		// Another process is established.
+		/* Here declares what sons are doing */
+		if(pid_2){
+			/* Declaration of Son process*/
 			printf("Son's getpid() = %d\n", getpid());
 			printf("Son's pid_1 = %d\n", pid_1);
 			printf("Son's pid_2 = %d\n", pid_2);
 			close(field[0]);
 			char * info = "Child 1 sends a message.";
 			write(field[1], info, strlen(info));
-			// printf("%s", info);
-
 		}
-		// Declaration of the actions of grandson
 		else{
+			/* Declaration of Grandson*/
 			printf("Grandson's getpid = %d\n", getpid());
 			printf("Grandson's pid_1 = %d\n", pid_1);
 			printf("Grandson's pid_2 = %d\n", pid_2);
 			close(field[0]);
 			char * info = "Child 2 sends a message.";
 			write(field[1], info, strlen(info));
-			// close(field[0]);
-			// char * son_info;
-			// strcpy(son_info, "Child 2 sends a message.");
-			// write(field[1], son_info, strlen(son_info));
 		}
 	}
 	return 0;
